3ee6392637b9c05eb6ac4683581b2fb9f73a4eaa
bepratham <prathamsinghkathayat@gmail.com>
2025-08-16 18:56:15 +0530

    Enumerations

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -1,3 +1,86 @@
 import UIKit
 
-var greeting = "Hello, playground"
+enum Animals{
+    case cat,dog,rabbit,hedgeHog
+}
+
+let cat = Animals.dog
+
+switch cat{
+case .cat:
+"This is a cat"
+break
+case .dog:
+"This is a dog"
+break
+case .rabbit:
+"This is a rabbit"
+break
+case .hedgeHog:
+"This is a hedgehog"
+break
+
+}
+enum Shortcut{
+    case fileOrFolder(path: URL, name: String)
+    case wwwUrl(path: URL)
+    case song(artist: String, songName: String)
+    
+}
+
+let appleWebsite = Shortcut.wwwUrl(path:  URL(string: "https://apple.com")!)
+
+switch appleWebsite{
+case.fileOrFolder(let url, let name):
+    print("url is \(url)")
+    break
+case.wwwUrl(let path):
+    print("url path is \(path)")
+    break
+default:
+    print("IDK")
+}
+ 
+if case let .wwwUrl(path) = appleWebsite{
+    path
+}
+
+let withoutYou = Shortcut.song(artist: "Symphony X", songName: "Without Y")
+
+if case let Shortcut.song(_, songName) = withoutYou{
+    songName
+}
+
+
+
+
+
+// Final
+
+
+enum Vehicle{
+    case car(manufacture: String, model: String)
+    case bike(manufacture: String, model: String)
+}
+
+let car = Vehicle.car(manufacture: "Tesla", model: "X")
+
+switch car{
+case let .car(manufacture, _):
+    manufacture
+    break
+case let .bike(manufacture,_):
+        manufacture
+    break
+}
+
+func getManufacture(from vehicle: Vehicle) -> String {
+    switch car {
+    case .car(let manufacture, let model):
+        return manufacture + " -> " + model
+    case .bike(let manufacture, let model):
+        return manufacture
+    }
+}
+
+print(getManufacture(from: car))
--- Structure.playground/Contents.swift
+++ Structure.playground/Contents.swift
@@ -6,3 +6,22 @@ struct Person{
 }
 let foo = Person(name: "foo", age: 9)
 
+struct Person2 {
+    let firstName: String
+    let lastName: String
+    var fullName: String{
+        "\(firstName) \(lastName) "
+    }
+}
+struct Person0 {
+    let firstName: String
+    let lastName: String
+    let fullName: String
+    init(firstName: String, lastName: String){
+            self.firstName = firstName
+            self.lastName = lastName
+            self.fullName = "\(firstName) \(lastName)"
+            
+        }
+}
+let n = Person0(firstName: "shanu", lastName: "singh")
