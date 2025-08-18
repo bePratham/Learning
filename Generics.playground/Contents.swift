998a3b2c2cec56aff8532057277de8be2e7d71d8
bepratham <prathamsinghkathayat@gmail.com>
2025-08-19 01:26:21 +0530

    Gen

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -14,3 +14,44 @@ func perform<N: Numeric>(
 
 perform(+, first: 10, second: 20)
 perform(*, first: 10.21, second: 22)
+
+protocol CAnRun{
+    func run()
+}
+protocol CAnJump{
+    func jump()
+}
+struct Person: CAnJump, CAnRun{
+    func jump(){
+        "jumping ..."
+    }
+    func run() {
+        "Running"
+    }
+}
+
+func jumpAndRun<T: CAnJump & CAnRun>(val: T){
+    val.run()
+    val.jump()
+}
+let person = Person()
+jumpAndRun(val: person)
+
+let nameList: Array<String> = ["Foo", "Bar"]
+//extension [String]̃
+
+var names : [String] = ["Shanu","Banu"]
+names.sort()
+names.sort(by: >)
+
+extension Array where Element == String{
+    func longestString() -> String?{
+        self.sorted {(lhs: String, rhs: String) -> Bool in lhs.count > rhs.count}.first
+    }
+}
+[
+    "Foo",
+    "Vbar vacx",
+    "Qux"
+].longestString()
+
